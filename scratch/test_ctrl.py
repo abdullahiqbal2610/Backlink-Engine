6c735feb2f3ad23d229872bd9d2a27b81f080d9c
Abdullah Iqbal <abdullahiqbal27122004@gmail.com>
2026-08-08 23:45:11 +0500

    Remove unnecessary test and poc files

--- scratch/test_ctrl.py
+++
@@ -1,4 +0,0 @@
-import inspect
-from browser_use import Controller
-print(dir(Controller))
-print(inspect.getsource(Controller))
--- scratch/test_ctx.py
+++
@@ -1,4 +0,0 @@
-import inspect
-from browser_use import Controller
-with open("controller_source.txt", "w", encoding="utf-8") as f:
-    f.write(inspect.getsource(Controller))
